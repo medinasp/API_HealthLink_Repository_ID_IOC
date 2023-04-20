841845ba1d41cff95e790a995a7c056c7663749b
medinasp <medinasp@gmail.com>
2023-04-19 22:16:48 -0300

    Tests Patient

---
+++ teste-mock.Tests/Controllers/PatientControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Interfaces;
+using HealthLinkApi.Controllers;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teste_mock.Tests.Controllers
+{
+    public class PatientControllerTests
+    {
+        private Mock<IPatient> mockPatientRepository;
+        private PatientController patientController;
+
+        public PatientControllerTests()
+        {
+            mockPatientRepository = new Mock<IPatient>();
+            patientController = new PatientController(mockPatientRepository.Object);
+        }
+    }
+}
