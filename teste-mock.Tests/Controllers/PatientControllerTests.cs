7b38e043d69b87b96f3454d4746d4a08358f7a21
medinasp <medinasp@gmail.com>
2023-04-19 22:38:59 -0300

    Tests Patient GetallAsync

--- teste-mock.Tests/Controllers/PatientControllerTests.cs
+++ teste-mock.Tests/Controllers/PatientControllerTests.cs
@@ -1,11 +1,14 @@
-﻿using Domain.Interfaces;
+﻿using Domain.Entities;
+using Domain.Interfaces;
 using HealthLinkApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace teste_mock.Tests.Controllers
 {
@@ -18,6 +21,29 @@ namespace teste_mock.Tests.Controllers
         {
             mockPatientRepository = new Mock<IPatient>();
             patientController = new PatientController(mockPatientRepository.Object);
+        }
+
+        [Fact]
+        public async Task GetAllAsync_ReturnsOkResult_WithListOfPatient()
+        {
+            //Arrange
+            List<Patient> patient = new List<Patient>
+            {
+                new Patient { Id = 1, Name = "Colie Porter"},
+                new Patient { Id = 2, Name = "Marvin Gaye"},
+                new Patient { Id = 3, Name = "BB King"},
+            };
+
+            mockPatientRepository.Setup(x => x.GetAllAsync()).ReturnsAsync(patient);
+
+            //Act
+            var result = await patientController.GetAllAsync();
+
+            //Assert
+            Assert.NotNull(result);
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var model = Assert.IsAssignableFrom<IEnumerable<Patient>> (okResult.Value);
+            Assert.Equal(patient.Count, model.Count());
         }
     }
 }
