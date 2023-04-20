a35978115f2ad314094f5718099710c531fac778
medinasp <medinasp@gmail.com>
2023-04-20 12:44:44 -0300

    Teste GetPatientsByDoctorIdAsync

--- teste-mock.Tests/Controllers/PatientControllerTests.cs
+++ teste-mock.Tests/Controllers/PatientControllerTests.cs
@@ -141,11 +141,34 @@ namespace teste_mock.Tests.Controllers
             string name = "Marvin";
 
             // Configurar o mock do IPatient para retornar uma lista fictícia de pacientes
-            var patients = new List<Patient> { new Patient { Id = 1, Name = "MarvinGaye" }, new Patient { Id = 2, Name = "BB King" } };
+            var patients = new List<Patient> { new Patient { Id = 1, Name = "Marvin Gaye" }, new Patient { Id = 2, Name = "BB King" } };
             mockPatientRepository.Setup(repo => repo.SearchByNameAsync(name)).ReturnsAsync(patients);
 
             // Act
             var result = await patientController.SearchByNameAsync(name);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<OkObjectResult>(result);
+            var okResult = result as OkObjectResult;
+            Assert.NotNull(okResult.Value);
+            var returnedPatients = okResult.Value as List<Patient>;
+            Assert.Equal(2, returnedPatients.Count);
+            // Faça mais verificações nos dados retornados, se necessário
+        }
+
+        [Fact]
+        public async Task GetPatientsByDoctorIdAsync_ReturnsOkResult_WithPatients()
+        {
+            // Arrange
+            int doctorId = 1;
+
+            // Configurar o mock do IPatient para retornar uma lista fictícia de pacientes
+            var patients = new List<Patient> { new Patient { Id = 1, Name = "Marvin Gaye" }, new Patient { Id = 2, Name = "BB King" } };
+            mockPatientRepository.Setup(repo => repo.GetPatientsByDoctorIdAsync(doctorId)).ReturnsAsync(patients);
+
+            // Act
+            var result = await patientController.GetPatientsByDoctorIdAsync(doctorId);
 
             // Assert
             Assert.NotNull(result);
